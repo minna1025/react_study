98953a2c4bdbe52f6a852dcef51aa1c294b8d7ce
NY.MIN <minna1025@gmail.com>
2018-11-20 01:06:08 +0900

    hello-react

---
+++ hello-react/src/MyName.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+// class MyName extends Component {
+// 	// static defaultProps = {
+// 	// 	name: '홍길동'
+// 	// }
+// 	render() {
+// 		return (
+// 			<div>
+// 				안녕하세요! 제 이름은 <b>{this.props.name}</b>입니다.
+// 			</div>
+// 		);
+// 	}
+// }
+
+const MyName = ({ name }) => {
+	return (
+		<div>
+			안녕하세요! 제 이름은 {name}입니당~
+		</div>
+	);
+}
+
+MyName.defaultProps = {
+	name: '홍길동_'
+};
+
+export default MyName;
